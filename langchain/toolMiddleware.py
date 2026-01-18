a589b9a57fbe352f6b5cdc80f9afc6da9466cc7a
“liaorunnan” <1742475562@qq.com>
2026-01-18 08:37:22 +0800

    langchain_中间件——重试

---
+++ langchain/toolMiddleware.py
@@ -0,0 +1,38 @@
+from langchain.agents import create_agent
+from langchain.agents.middleware import ToolRetryMiddleware
+from langchain.tools import  tool
+from langchain_openai import ChatOpenAI
+from conf import settings
+
+model = ChatOpenAI(temperature=0.1, model_name=settings.qw_model, api_key=settings.qw_api_key, base_url=settings.qw_api_url,timeout=1)
+
+
+
+@tool
+def get_user_info():
+    """
+    获取用户信息
+    """
+    print("获取用户信息")
+    time.sleep(2)
+    print("获取用户信息完成")
+    return {
+        "name": "李四",
+    }
+
+agent = create_agent(
+    model=model,
+    tools=[get_user_info],
+    middleware=[
+        ToolRetryMiddleware(
+            max_retries=3,
+            backoff_factor=2.0,
+            initial_delay=1.0,
+        ),
+    ],
+)
+
+message = agent.invoke(
+    {"messages": [{"role": "user", "content": "你好,我的名字是什么"}]},
+)
+print(message['messages'][-1].content)
